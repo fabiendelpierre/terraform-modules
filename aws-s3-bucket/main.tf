e5b9a36e86a1abf69196f4433390482aa6da5e0f
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2023-09-27 17:01:11 -0400

    S3 bucket ACLs are too passé

--- aws-s3-bucket/main.tf
+++ aws-s3-bucket/main.tf
@@ -9,9 +9,13 @@ resource "aws_s3_bucket" "main" {
   )
 }
 
-resource "aws_s3_bucket_acl" "main" {
+resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
-  acl    = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" {

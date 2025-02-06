12db4188a616165c147bd0d0a63f8160163e8c86
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2025-02-06 11:41:43 +0100

    fix: syntax

--- aws-ssm-vpc-endpoints/main.tf
+++ aws-ssm-vpc-endpoints/main.tf
@@ -8,46 +8,38 @@ resource "aws_security_group" "main" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = data.aws_vpc.main.cidr_block
+    cidr_blocks = [data.aws_vpc.main.cidr_block]
+  }
 
-  }
-  tags = {
-    Name = merge({ Name = "${data.aws_vpc.main.tags.Name}-ssm-vpc-endpoints" }, var.tags)
-  }
+  tags = merge({ Name = "${data.aws_vpc.main.tags.Name}-ssm-vpc-endpoints" }, var.tags)
 }
 
 resource "aws_vpc_endpoint" "ssm_endpoint" {
-  vpc_id              = data.aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = var.subnet_ids
-  security_group_ids  = [aws_security_group.main.id]
+  vpc_id             = data.aws_vpc.main.id
+  service_name       = "com.amazonaws.${data.aws_region.current.name}.ssm"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = var.subnet_ids
+  security_group_ids = [aws_security_group.main.id]
 
-  tags = {
-    Name = merge({ Name = "${data.aws_vpc.main.tags.Name}-ssm" }, var.tags)
-  }
+  tags = merge({ Name = "${data.aws_vpc.main.tags.Name}-ssm" }, var.tags)
 }
 
 resource "aws_vpc_endpoint" "ec2_messages_endpoint" {
-  vpc_id              = data.aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = var.subnet_ids
-  security_group_ids  = [aws_security_group.main.id]
+  vpc_id             = data.aws_vpc.main.id
+  service_name       = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = var.subnet_ids
+  security_group_ids = [aws_security_group.main.id]
 
-  tags = {
-    Name = merge({ Name = "${data.aws_vpc.main.tags.Name}-ec2messages" }, var.tags)
-  }
+  tags = merge({ Name = "${data.aws_vpc.main.tags.Name}-ec2messages" }, var.tags)
 }
 
 resource "aws_vpc_endpoint" "ssm_messages_endpoint" {
-  vpc_id              = data.aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = var.subnet_ids
-  security_group_ids  = [aws_security_group.main.id]
+  vpc_id             = data.aws_vpc.main.id
+  service_name       = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = var.subnet_ids
+  security_group_ids = [aws_security_group.main.id]
 
-  tags = {
-    Name = merge({ Name = "${data.aws_vpc.main.tags.Name}-ssmmessages" }, var.tags)
-  }
+  tags = merge({ Name = "${data.aws_vpc.main.tags.Name}-ssmmessages" }, var.tags)
 }
